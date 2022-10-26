d077b20697f53fd0e14d3796da512585d19a0424
MD.Azad Hossain Razu <110036232+Azad507@users.noreply.github.com>
2022-10-26 09:43:13 +0600

    Add files via upload

---
+++ Lab 04/9.507_49_10.cpp
@@ -0,0 +1,20 @@
+
+//Codeforces Problem No - 231A
+//Problame name - Team#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int arr[a],countn=0;
+    for(int i=0;i++<a;)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i++<a;)
+    {
+        arr[i]>=arr[b]&&arr[i]!=0?countn++:countn;
+    }
+    cout<<countn<<endl;
+    return 0;
+}
